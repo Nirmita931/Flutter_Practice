acac4f3c2be087d010ef7636cafb4d6d9ee889fc
nirmita <nkc@sevadev.com>
2020-09-05 17:48:31 +0545

    World time application all

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,12 @@ import 'package:world_time/pages/home.dart';
 import 'package:world_time/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/',
-  routes: {
-    '/' : (context) => Loading(),
-    '/home' : (context) => Home(),
-    '/location' : (context) => ChooseLocation(),
-  },
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }
 ));
 
 
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,28 +1,37 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime{
   String location; // Location name for the UI
   String time; // Time in that location
   String flag; // URl to an asset flag icon
   String url; // Location URL for API endpoint
-
+  bool isDayTime;
   WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async{
-    // Make the request
-    Response response =  await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
 
-    // Get properties from data
-    String datetime = data['utc_datetime'];
-    String offset = data['utc_offset'].substring(1,3);
+    try{
+      // Make the request
+      Response response =  await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
 
-    // Create a datetime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      // Get properties from data
+      String datetime = data['utc_datetime'];
+      String offset = data['utc_offset'].substring(1,3);
 
-    // Set the time property
-    time = now.toString();
+      // Create a datetime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      // Set the time property
+      isDayTime = now.hour > 6 && now.hour < 20 ? true : false;
+      time = DateFormat.jm().format(now);
+    }
+    catch(e){
+      print('Caught error: $e');
+      time = 'Could not get time data';
+    }
   }
 }
